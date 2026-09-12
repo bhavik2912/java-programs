334b6aa8933046fcc6ce76c6617172409aaeb23d
bhavik2912 <bhavikjain29122006@gmail.com>
2026-09-13 01:07:54 +0530

    Add class and object example

---
+++ oop/ClassObject.java
@@ -0,0 +1,19 @@
+class Student {
+    String name;
+    int age;
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class ClassObject {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Bhavik";
+        s1.age = 20;
+
+        s1.display();
+    }
+}
